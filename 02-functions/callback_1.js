b7ad8af4788977d4cf2ce99923fe3923ccd2e6b9
william koller <wkoller25@gmail.com>
2021-07-21 09:51:56 -0300

    feat: add callback 01

--- 02-functions/callback_1.js
+++ 02-functions/callback_1.js
@@ -16,4 +16,5 @@ exec(subtrairNoTerminal, 182, 27);
 const r = exec(subtrair, 50, 25);
 console.log(r)
 
-setInterval(() => console.log('Exec...'), 1000);
+const cb = () => console.log('Exec.....');
+setInterval(cb, 1000);
